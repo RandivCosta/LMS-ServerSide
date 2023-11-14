fe8a0b218a0726fa799a46cddef82171ce4f3a2e
RandivCosta <venukrandivcosta1234@gmail.com>
2023-11-14 21:50:44 +0530

    user login update

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -7,6 +7,7 @@ import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 
 // register user
 
@@ -128,13 +129,13 @@ interface ILoginRequest {
 
 export const loginUser = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {email,password} = req.body as ILoginRequest;
+        const { email, password } = req.body as ILoginRequest;
 
-        if(!email || !password) {
+        if (!email || !password) {
             return next(new ErrorHandler("Please enter email and password", 400));
         };
 
-        const user = await userModel.findOne({email}).select("+password");
+        const user = await userModel.findOne({ email }).select("+password");
 
         if (!user) {
             return next(new ErrorHandler("Invalid email or password", 400));
@@ -145,7 +146,23 @@ export const loginUser = CatchAsyncError(async (req: Request, res: Response, nex
             return next(new ErrorHandler("Invalid email or password", 400));
         };
 
+        sendToken(user, 200, res);
+
     } catch (error: any) {
-        return next(new ErrorHandler(error.message, 400))
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+// logout user
+export const logoutUser = CatchAsyncError(async(req: Request,res: Response, next: NextFunction) => {
+    try{
+        res.cookie("access_token", "", {maxAge: 1});
+        res.cookie("refresh_token", "", {maxAge: 1});
+        res.status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+    } catch (error: any) {
+        return next(new ErrorHandler(error.message, 400));
     }
 });
